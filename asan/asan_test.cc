4b4d0758b01355e083b258ee120b7b5554af7cce
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-05-05 09:19:19 +0000

    fix OutOfMemoryTest for llvm/clang

--- asan/asan_test.cc
+++ asan/asan_test.cc
@@ -226,8 +226,8 @@ TEST(AddressSanitizer, IgnoreTest) {
 }
 
 TEST(AddressSanitizer, OutOfMemoryTest) {
-  size_t size = __WORDSIZE == 64 ? (1ULL << 40) : (0xf0000000);
-  EXPECT_DEATH(void *p = malloc(size),
+  size_t size = __WORDSIZE == 64 ? (size_t)(1ULL << 40) : (0xf0000000);
+  EXPECT_DEATH(printf("%p\n", malloc(size)),
                "ERROR: AddressSanitizer failed to allocate.*main memory");
 }
 
