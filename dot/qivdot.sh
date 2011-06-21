e094cbdd7df70a7892432828cdfc89244f04f893
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-06-21 15:37:33 +0000

    remove dot

--- dot/qivdot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-png=`basename $1 .dot`.png
-dot -Tpng $1 > $png
-qiv $png
-rm -f $png
--- dot/render_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for fff in *.dot; do
-  dot -Tpng $fff > `basename $fff .dot`.png &
-done
-wait
