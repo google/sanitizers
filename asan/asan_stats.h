e85901ad8254e66f9425bf8f7b8666370c242d31
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-06-21 12:44:17 +0000

    add asan_stats.h

---
+++ asan/asan_stats.h
@@ -0,0 +1,39 @@
+/* Copyright 2011 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// This file is a part of AddressSanitizer, an address sanity checker.
+
+#ifndef ASAN_STATS_H
+#define ASAN_STATS_H
+struct AsanStats {
+  size_t low_shadow_maps;
+  size_t high_shadow_maps;
+  size_t mallocs;
+  size_t malloced;
+  size_t malloced_redzones;
+  size_t frees;
+  size_t freed;
+  size_t real_frees;
+  size_t really_freed;
+  size_t reallocs;
+  size_t realloced;
+  size_t freed_since_last_stats;
+
+  void PrintStats();
+};
+
+extern AsanStats __asan_stats;
+
+#endif  // ASAN_STATS_H
