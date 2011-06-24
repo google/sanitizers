3f58ce2187645ec494b5d350df684778e6a0e6e9
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-06-24 12:11:42 +0000

    added output tests

---
+++ asan/tests/test_output.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OS=`uname`
+CXX=../../asan_clang_$OS/bin/clang++
+SYMBOLIZER=../../scripts/asan_symbolize.py
+
+for t in  *.tmpl; do
+  for b in 32 64; do 
+    c=`basename $t .tmpl`
+    echo $b $c
+    $CXX -g -m$b -fasan -O2 $c.cc
+    ./a.out 2>&1 | $SYMBOLIZER | ./match_output.py $t
+  done
+done
