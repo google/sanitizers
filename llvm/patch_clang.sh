e4266f6cc261e347767ad3fc42855b5daf5eec75
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-06-27 13:03:10 +0000

    don't use ignore.cc and common_util.cc

--- llvm/patch_clang.sh
+++ llvm/patch_clang.sh
@@ -6,9 +6,3 @@ cd lib/Transforms/Instrumentation
 ROOT=../../../..
 ln -fs $ROOT/llvm/AddressSanitizer.cpp
 ln -fs $ROOT/asan/asan_rtl.h
-ln -fs $ROOT/third_party/tsan/common_util.cc
-ln -fs $ROOT/third_party/tsan/common_util.h
-ln -fs $ROOT/third_party/tsan/ignore.cc
-ln -fs $ROOT/third_party/tsan/ignore.h
-ln -fs $ROOT/third_party/tsan/ts_util.h
-
