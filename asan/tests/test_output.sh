067ff2da1b2123528794495c9224507564d48126
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-07-04 09:53:00 +0000

    one more output test; run all output tests in parallel

--- asan/tests/test_output.sh
+++ asan/tests/test_output.sh
@@ -5,11 +5,15 @@ CXX=../../clang_build_$OS/Release+Asserts/bin/clang++
 SYMBOLIZER=../../scripts/asan_symbolize.py
 
 for t in  *.tmpl; do
-  for b in 32 64; do 
+  for b in 32 64; do
     c=`basename $t .tmpl`
-    echo $b $c
-    $CXX -g -m$b -fasan -O2 $c.cc
-    ./a.out 2>&1 | $SYMBOLIZER | c++filt | ./match_output.py $t || exit 1
-    rm ./a.out
+    exe=$c.$b
+    (
+    $CXX -g -m$b -fasan -O2 $c.cc -o $exe
+    ./$exe 2>&1 | $SYMBOLIZER 2> /dev/null | c++filt | ./match_output.py $t || exit 1
+    echo $exe
+    rm ./$exe
+    ) &
   done
 done
+wait
