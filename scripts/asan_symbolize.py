a1abeca456b02e7cb042136fa4aacc7f7795bfb5
ramosian.glider <ramosian.glider@gmail.com>
2011-07-07 11:56:13 +0000

    Support for Mac OS using atos

--- scripts/asan_symbolize.py
+++ scripts/asan_symbolize.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
+import os
 import re
 import sys
 import string
 import subprocess
 
-addr2line_pipes = dict()
+pipes = {}
 
-for line in sys.stdin:
+# TODO(glider): need some refactoring here
+def symbolize_addr2line(line):
   #0 0x7f6e35cf2e45  (/blah/foo.so+0x11fe45)
   match = re.match('^( *#[0-9]+ *0x[0-9a-f]+) *\((.*)\+(0x[0-9a-f]+)\)', line)
   if match:
     binary = match.group(2)
     addr = match.group(3)
-    if not addr2line_pipes.has_key(binary):
-      addr2line_pipes[binary] = subprocess.Popen(["addr2line", "-f", "-e", binary],
+    if not pipes.has_key(binary):
+      print ["addr2line", "-f", "-e", binary]
+      pipes[binary] = subprocess.Popen(["addr2line", "-f", "-e", binary],
                          stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    p = addr2line_pipes[binary]
+    p = pipes[binary]
     try:
       print >>p.stdin, addr
+      print addr
       function_name = p.stdout.readline().rstrip()
       file_name     = p.stdout.readline().rstrip()
     except:
@@ -31,3 +35,42 @@ for line in sys.stdin:
     print match.group(1), "in", function_name, file_name
   else:
     print line.rstrip()
+
+def symbolize_atos(line):
+  #0 0x7f6e35cf2e45  (/blah/foo.so+0x11fe45)
+  match = re.match('^( *#[0-9]+ *)(0x[0-9a-f]+) *\((.*)\+(0x[0-9a-f]+)\)', line)
+  if match:
+    #print line
+    prefix = match.group(1)
+    addr = match.group(2)
+    binary = match.group(3)
+    offset = match.group(4)
+    load_addr = int(addr, 16) - int(offset, 16)
+    if not pipes.has_key(binary):
+      #print "atos -o %s -l %s" % (binary, hex(load_addr))
+      pipes[binary] = subprocess.Popen(["atos", "-o", binary],
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,)
+    p = pipes[binary]
+    # TODO(glider): how to tell if the address is absolute?
+    if ".app/" in binary and not ".framework" in binary:
+      print >>p.stdin, "%s" % addr
+    else:
+      print >>p.stdin, "%s" % offset
+    # TODO(glider): it's more efficient to make a batch atos run for each binary.
+    p.stdin.close()
+    atos_line = p.stdout.readline().rstrip()
+    del pipes[binary]
+
+    print "%s%s in %s" % (prefix, addr, atos_line)
+  else:
+    print line.rstrip()
+
+system = os.uname()[0]
+if system in ['Linux', 'Darwin']:
+  for line in sys.stdin:
+    if system == 'Linux':
+      symbolize_addr2line(line)
+    elif system == 'Darwin':
+      symbolize_atos(line)
+else:
+  print 'Unknown system: ', system
