0e0dc395d236ebcf3fb6d8bd16fb3fc11ceb18d4
ramosian.glider <ramosian.glider@gmail.com>
2011-07-22 17:07:09 +0000

    Add a shared library test. This is not automated yet, but already exposes a problem with globals in .so libraries.
    To build:
      g++ shared-lib-main.cc -m32 -o shared-lib-main32 -ldl
      g++ shared-lib-so.cc -m32 -o shared-lib-so32.so -shared

---
+++ asan/tests/shared-lib-main.cc
@@ -0,0 +1,35 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+// Author: glider@google.com (Alexander Potapenko)
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <string>
+
+using namespace std;
+
+const char kMyName[] = "shared-lib-main";
+const char kSoName[] = "shared-lib-so";
+
+typedef void (fun_t)(int);
+
+int main(int argc, char *argv[]) {
+  string path = strdup(argv[0]);
+  size_t start = path.find(kMyName);
+  if (start == string::npos) return 1;
+  path.replace(start, strlen(kMyName), kSoName);
+  path += ".so";
+  printf("opening %s ... ", path.c_str());
+  void *lib = dlopen(path.c_str(), RTLD_NOW);
+  if (!lib) {
+    printf("error in dlopen(): %s\n", dlerror());
+    return 1;
+  }
+  fun_t *inc = (fun_t*)dlsym(lib, "inc");
+  if (!inc) return 1;
+  printf("ok\n");
+  inc(1);
+//  inc(-1);
+  return 0;
+}
---
+++ asan/tests/shared-lib-so.cc
@@ -0,0 +1,12 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+// Author: glider@google.com (Alexander Potapenko)
+
+#include <stdio.h>
+
+int GLOB[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+extern "C"
+void inc(int index) {
+  GLOB[index]++;
+  printf("GLOB[%d]: %d\n", index, GLOB[index]);
+}
