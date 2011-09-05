e8e6f3a9e51a38ffe4afcb4bf90b1feb76d9e056
ramosian.glider <ramosian.glider@gmail.com>
2011-09-05 13:56:26 +0000

    Update the Clang version used by ASan to r139006 This fixes the problem with misaligned data passed to SSE movaps instruction.

--- llvm/get_clang.sh
+++ llvm/get_clang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LLVM_REV=135664
+LLVM_REV=139006
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/llvm/trunk clang_src
 cd clang_src/tools
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/cfe/trunk clang
