db69a0655e513c3d4195eef794d5df77b8a22711
ramosian.glider <ramosian.glider@gmail.com>
2011-09-07 07:48:39 +0000

    Splitted the LLVM revision into a separate config file to allow using it in a CrOS ebuild

---
+++ llvm/config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LLVM_REV=139006
--- llvm/get_clang.sh
+++ llvm/get_clang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LLVM_REV=139006
+. config.sh
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/llvm/trunk clang_src
 cd clang_src/tools
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/cfe/trunk clang
