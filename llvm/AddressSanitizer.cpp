c94d53562caf37ba654b8a9c930b5379c2c152b5
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-09-14 22:10:05 +0000

    don't add offset if it is zero (just in case)

--- llvm/AddressSanitizer.cpp
+++ llvm/AddressSanitizer.cpp
@@ -262,6 +262,8 @@ static void CloneDebugInfo(Instruction *From, Instruction *To) {
 Value *AddressSanitizer::memToShadow(Value *Shadow, IRBuilder<> &IRB) {
   // Shadow >> scale
   Shadow = IRB.CreateLShr(Shadow, MappingScale);
+  if (MappingScale == 0)
+    return Shadow;
   // (Shadow >> scale) | offset
   return IRB.CreateOr(Shadow, ConstantInt::get(IntptrTy,
                                                MappingOffset));
