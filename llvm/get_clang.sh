8302ac61e303d909a2c61edef58254e0c6dd0543
ramosian.glider <ramosian.glider@gmail.com>
2011-09-20 01:48:16 +0000

    Fix the problem with relative path to config.sh

--- llvm/get_clang.sh
+++ llvm/get_clang.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-. config.sh
+export THISDIR=`dirname $0`
+. $THISDIR/config.sh
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/llvm/trunk clang_src
 cd clang_src/tools
 svn co -r$LLVM_REV http://llvm.org/svn/llvm-project/cfe/trunk clang
