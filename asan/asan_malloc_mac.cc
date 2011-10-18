7b97262f42c1737bc9cad6cbfaf950fc18754198
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2011-10-18 23:51:38 +0000

    fix mac run (sort of)

--- asan/asan_malloc_mac.cc
+++ asan/asan_malloc_mac.cc
@@ -272,11 +272,11 @@ void mi_log(malloc_zone_t *zone, void *address) {
 }
 
 void mi_force_lock(malloc_zone_t *zone) {
-  UNIMPLEMENTED();
+  // UNIMPLEMENTED();
 }
 
 void mi_force_unlock(malloc_zone_t *zone) {
-  UNIMPLEMENTED();
+  // UNIMPLEMENTED();
 }
 
 void mi_statistics(malloc_zone_t *zone, malloc_statistics_t *stats) {
@@ -289,7 +289,7 @@ void mi_statistics(malloc_zone_t *zone, malloc_statistics_t *stats) {
 }
 
 boolean_t mi_zone_locked(malloc_zone_t *zone) {
-  UNIMPLEMENTED();
+  // UNIMPLEMENTED();
   return false;
 }
 
