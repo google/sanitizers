441dcaa0462f406b25c7e088b3fc5b560f3159e7
ramosian.glider <ramosian.glider@gmail.com>
2011-10-18 12:40:31 +0000

    Decrease the number of jobs on Mac. It's anyway incorrect to have the number of jobs hardcoded.

--- llvm/rebuild_clang_and_asan.sh
+++ llvm/rebuild_clang_and_asan.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 OS=`uname`
 DIR=clang_build_$OS
-(cd $DIR/lib/Transforms/Instrumentation/ && make -j16 ENABLE_OPTIMIZED=1) && \
-(cd $DIR/tools/clang/ && make -j16 ENABLE_OPTIMIZED=1)
+JOBS=-j16
+if [ "$OS" == "Darwin" ]
+then
+  JOBS=-j8
+fi
+(cd $DIR/lib/Transforms/Instrumentation/ && make $JOBS ENABLE_OPTIMIZED=1) && \
+(cd $DIR/tools/clang/ && make $JOBS ENABLE_OPTIMIZED=1)
