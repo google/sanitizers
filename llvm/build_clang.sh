3ddce9c1e1c1a6862d1412f78fb6d4ed3998d25f
ramosian.glider <ramosian.glider@gmail.com>
2011-11-02 10:01:26 +0000

    Use sysctl to obtain the number of CPUs on Darwin.

--- llvm/build_clang.sh
+++ llvm/build_clang.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 OS=`uname`
 DIR=clang_build_$OS
+JOBS=-j16
+if [ "$OS" == "Darwin" ]
+then
+  JOBS=-j`sysctl -n hw.logicalcpu`
+fi
 rm -rf $DIR
 mkdir -p $DIR
 cd $DIR
 ../clang_src/configure --enable-optimized
-make -j ${J:-16}
+make $JOBS
 
--- llvm/rebuild_clang_and_asan.sh
+++ llvm/rebuild_clang_and_asan.sh
@@ -4,7 +4,7 @@ DIR=clang_build_$OS
 JOBS=-j16
 if [ "$OS" == "Darwin" ]
 then
-  JOBS=-j8
+  JOBS=-j`sysctl -n hw.logicalcpu`
 fi
 (cd $DIR/lib/Transforms/Instrumentation/ && make $JOBS ENABLE_OPTIMIZED=1) && \
 (cd $DIR/tools/clang/ && make $JOBS ENABLE_OPTIMIZED=1)
