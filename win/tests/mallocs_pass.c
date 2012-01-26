2162512e7d9df72b40ad7fd2424712a157d2753f
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2012-01-26 13:54:58 +0000

    Add a few more malloc tests

---
+++ win/tests/mallocs_pass.c
@@ -0,0 +1,31 @@
+/* Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This file is a part of AddressSanitizer, an address sanity checker.
+
+#include "common.h"
+
+int main(void) {
+  int *p = (int*)malloc(1024 * sizeof(int));
+  p[512] = 0;
+  free(p);
+
+  p = (int*)malloc(128);
+  p = (int*)realloc(p, 2048 * sizeof(int));
+  p[1024] = 0;
+  free(p);
+
+  return 0;
+}
