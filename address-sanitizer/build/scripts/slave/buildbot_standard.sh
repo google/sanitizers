559c36c5d9536cf52fdb7415de8d2be3e3ab808f
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2012-02-03 12:43:28 +0000

    Fix misused bashisms in buildbot_standard.sh

--- address-sanitizer/build/scripts/slave/buildbot_standard.sh
+++ address-sanitizer/build/scripts/slave/buildbot_standard.sh
@@ -9,12 +9,12 @@ echo @@@BUILD_STEP clobber@@@
 rm -rf llvm-build
 
 echo @@@BUILD_STEP update@@@
-if [ "x$BUILDBOT_CLOBBER" != "x" ]; then
+if [ "$BUILDBOT_CLOBBER" != "" ]; then
   rm -rf llvm
 fi
 
 REV_ARG=
-if [ "x$BUILDBOT_REVISION" != "x" ]; then
+if [ "$BUILDBOT_REVISION" != "" ]; then
   REV_ARG="-r$BUILDBOT_REVISION"
 fi
 
@@ -22,14 +22,14 @@ MAKE_JOBS=${MAX_MAKE_JOBS:-16}
 
 if [ -d llvm ]; then
   svn up llvm $REV_ARG
-  if [ "x$REV_ARG" == "x" ]; then
+  if [ "$REV_ARG" == "" ]; then
     REV_ARG="-r"$(svn info llvm | grep '^Revision:' | awk '{print $2}')
   fi
   svn up llvm/tools/clang $REV_ARG
   svn up llvm/projects/compiler-rt $REV_ARG
 else
   svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm $REV_ARG
-  if [ "x$REV_ARG" == "x" ]; then
+  if [ "$REV_ARG" == "" ]; then
     REV_ARG="-r"$(svn info llvm | grep '^Revision:' | awk '{print $2}')
   fi
   svn co http://llvm.org/svn/llvm-project/cfe/trunk llvm/tools/clang $REV_ARG
