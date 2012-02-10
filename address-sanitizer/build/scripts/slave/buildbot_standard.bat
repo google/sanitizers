36fc7ab1daa72f60677b8f9b094ca80cea024d5e
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2012-02-10 14:48:36 +0000

    Win bot: build Clang, run tests

--- address-sanitizer/build/scripts/slave/buildbot_standard.bat
+++ address-sanitizer/build/scripts/slave/buildbot_standard.bat
@@ -5,28 +5,47 @@
 echo @@@BUILD_STEP update@@@
 :: TODO(timurrrr)
 ::if [ "$BUILDBOT_CLOBBER" != "" ]; then
-rmdir /S /Q asan
+::rmdir /S /Q asan
 ::fi
 
 set REV_ARG=
 if NOT "%BUILDBOT_REVISION%" == "" set REV_ARG="-r%BUILDBOT_REVISION%"
 
-:: TODO(timurrrr): checkout more
-
 :: call -> because "svn" might be a batch script, ouch
-call svn checkout http://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/asan asan %REV_ARG%
-cd asan
+call svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm %REV_ARG% || goto :DIE
+call svn co http://llvm.org/svn/llvm-project/cfe/trunk llvm/tools/clang %REV_ARG% || goto :DIE
+call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/asan asan_rtl %REV_ARG% || goto :DIE
+call svn co http://address-sanitizer.googlecode.com/svn/trunk/win/tests win_tests || goto :DIE
 
-:: TODO(timurrrr) echo @@@BUILD_STEP build llvm@@@
+@@@BUILD_STEP build llvm@@@
+rmdir /S /Q llvm-build
+mkdir llvm-build || goto :DIE
+cd llvm-build
+:: TODO(timurrrr) make this incremental?
+cmake ..\llvm || goto :DIE
+devenv LLVM.sln /Build Debug /Project clang || goto :DIE
+cd ..
 
 :: TODO(timurrrr) echo @@@BUILD_STEP test llvm@@@
 
-echo @@@BUILD_STEP build asan@@@
-:: TODO(timurrrr): this only tests that this compiles, not links.
-cl /nologo /Zi /c *.cc
+echo @@@BUILD_STEP build asan RTL@@@
+cd asan_rtl
+:: This only compiles, not links.
+del *.obj || goto :DIE
+cl /nologo /MP /Zi /c *.cc || goto :DIE
+cd ..
 
-:: TODO(timurrrr) echo @@@BUILD_STEP asan test32@@@
+echo @@@BUILD_STEP asan test@@@
+cd win_tests
+C:\cygwin\bin\make PLATFORM=Windows CC=../llvm-build/bin/Debug/clang.exe CC_OUT="-c -o" CFLAGS=-faddress-sanitizer EXTRA_OBJ=../asan_rtl/*.obj || goto :DIE
+cd ..
 
 :: TODO(timurrrr) echo @@@BUILD_STEP asan test64@@@
 
 :: TODO(timurrrr) echo @@@BUILD_STEP asan output_tests@@@
+
+echo "ALL DONE"
+goto :EOF
+
+:DIE
+exit /b %ERRORLEVEL%
