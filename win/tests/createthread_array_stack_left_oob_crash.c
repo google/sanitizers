830425cab3cbfdca1c25d3183d8b52ae8e12416b
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2012-02-20 09:46:24 +0000

    Add a couple of thread-stack-OOB tests

---
+++ win/tests/createthread_array_stack_left_oob_crash.c
@@ -0,0 +1,35 @@
+/* Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This file is a part of AddressSanitizer, an address sanity checker.
+
+#include <windows.h>
+
+#include "common.h"
+
+DWORD WINAPI thread_proc(void *context) {
+  int subscript = -1;
+  char stack_buffer[42];
+  stack_buffer[subscript] = 42;
+  return 0;
+}
+
+int main(void) {
+  DWORD tid = -1;
+  HANDLE thr = CreateThread(NULL, 0, thread_proc, NULL, 0, &tid);
+  CHECK(thr > 0);
+  CHECK(WAIT_OBJECT_0 == WaitForSingleObject(thr, INFINITE));
+  return 0;
+}
