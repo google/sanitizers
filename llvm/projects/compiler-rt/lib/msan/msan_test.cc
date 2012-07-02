e844134dc4cdfb4750a900de177aae69d85bd765
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2012-07-02 17:38:53 +0400

    test commit

--- llvm/projects/compiler-rt/lib/msan/msan_test.cc
+++ llvm/projects/compiler-rt/lib/msan/msan_test.cc
@@ -330,7 +330,7 @@ TEST(MemorySanitizer, fread) {
 }
 
 extern "C" {
-NOINLINE void ZZZZZZZZZZZZZZZ() {
+NOINLINE void ZZZZZZZZZZZZZZ() {
   __msan_break_optimization(0);
 
   // v_s1 = ReturnPoisoned<S1>();
@@ -341,7 +341,7 @@ NOINLINE void ZZZZZZZZZZZZZZZ() {
 }
 
 TEST(MemorySanitizer, ZZZTest) {
-  ZZZZZZZZZZZZZZZ();
+  ZZZZZZZZZZZZZZ();
 }
 
 int main(int argc, char **argv) {
