2a2279fa3c0e0291e0ee6ad3f1e853cfeca97b69
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2012-08-29 11:47:05 +0000

    Fix the Win bot?

--- address-sanitizer/build/scripts/slave/buildbot_standard.bat
+++ address-sanitizer/build/scripts/slave/buildbot_standard.bat
@@ -6,9 +6,9 @@ echo @@@BUILD_STEP update@@@
 :: TODO(timurrrr)
 ::if [ "$BUILDBOT_CLOBBER" != "" ]; then
 ::  echo @@@BUILD_STEP clobber build@@@
-::  rmdir /S /Q asan_rtl || goto :DIE
 ::  rmdir /S /Q llvm || goto :DIE
 ::  rmdir /S /Q llvm-build || goto :DIE
+::  rmdir /S /Q compiler_rt || goto :DIE
 ::  mkdir llvm-build || goto :DIE
 ::  rmdir /S /Q win_tests || goto :DIE
 ::fi
@@ -19,7 +19,7 @@ if NOT "%BUILDBOT_REVISION%" == "" set REV_ARG="-r%BUILDBOT_REVISION%"
 :: call -> because "svn" might be a batch script, ouch
 call svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm %REV_ARG% || goto :DIE
 call svn co http://llvm.org/svn/llvm-project/cfe/trunk llvm/tools/clang %REV_ARG% || goto :DIE
-call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk/lib rtl %REV_ARG% || goto :DIE
+call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler_rt %REV_ARG% || goto :DIE
 call svn co http://address-sanitizer.googlecode.com/svn/trunk/win/tests win_tests || goto :DIE
 
 mkdir llvm-build
@@ -37,7 +37,7 @@ cd ..
 :: TODO(timurrrr) echo @@@BUILD_STEP test llvm@@@
 
 echo @@@BUILD_STEP build asan RTL@@@
-cd rtl\asan || goto :DIE
+cd compiler_rt\lib\asan || goto :DIE
 :: This only compiles, not links.
 del *.pdb *.obj *.lib || goto :DIE
 
@@ -46,11 +46,11 @@ del *.pdb *.obj *.lib || goto :DIE
 :: /Zi <- generate debug info
 cl /nologo /MP /MT /Zi /I.. /I../../include /c *.cc ../interception/*.cc ../sanitizer_common/*.cc || goto :DIE
 lib /nologo /OUT:asan_rtl.lib *.obj || goto :DIE
-cd ..\..
+cd ..\..\..
 
 echo @@@BUILD_STEP asan test@@@
 cd win_tests || goto :DIE
-C:\cygwin\bin\make -s PLATFORM=Windows CC=../llvm-build/bin/Debug/clang++.exe FILECHECK=../llvm-build/bin/Debug/FileCheck.exe CFLAGS="-faddress-sanitizer -Xclang -cxx-abi -Xclang microsoft -g" EXTRA_OBJ=../rtl/asan/asan_rtl.lib || goto :DIE
+C:\cygwin\bin\make -s PLATFORM=Windows CC=../llvm-build/bin/Debug/clang++.exe FILECHECK=../llvm-build/bin/Debug/FileCheck.exe CFLAGS="-faddress-sanitizer -Xclang -cxx-abi -Xclang microsoft -g" EXTRA_OBJ=../compiler_rt/lib/asan/asan_rtl.lib || goto :DIE
 cd ..
 
 :: TODO(timurrrr) echo @@@BUILD_STEP asan test64@@@
