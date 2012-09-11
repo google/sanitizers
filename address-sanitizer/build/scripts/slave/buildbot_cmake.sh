7a598fbd9ead9fcafc76e50f174a587bf6b60181
samsonov <samsonov@google.com>
2012-09-11 11:09:34 +0000

    Add check-sanitizer command to CMake buildbot (and refactor buildbot script a bit)

--- address-sanitizer/build/scripts/slave/buildbot_cmake.sh
+++ address-sanitizer/build/scripts/slave/buildbot_cmake.sh
@@ -45,9 +45,7 @@ if [ ! -d llvm_build64 ]; then
   mkdir llvm_build64
   (cd llvm_build64 && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE $LLVM_CHECKOUT)
 fi
-cd llvm_build64
-make -j$MAKE_JOBS
-cd $ROOT
+(cd llvm_build64 && make -j$MAKE_JOBS)
 
 echo @@@BUILD_STEP build 32-bit llvm@@@
 if [ ! -d llvm_build32 ]; then
@@ -55,46 +53,43 @@ if [ ! -d llvm_build32 ]; then
   (cd llvm_build32 && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
                             -DLLVM_BUILD_32_BITS=ON $LLVM_CHECKOUT)
 fi
-cd llvm_build32
-make -j$MAKE_JOBS
-cd $ROOT
+(cd llvm_build32 && make -j$MAKE_JOBS)
 
 echo @@@BUILD_STEP lint@@@
 CHECK_LINT=${LLVM_CHECKOUT}/projects/compiler-rt/lib/sanitizer_common/scripts/check_lint.sh
 (LLVM_CHECKOUT=${LLVM_CHECKOUT} ${CHECK_LINT}) || echo @@@STEP_WARNINGS@@@
 
-echo @@@BUILD_STEP build asan tests@@@
 ASAN_PATH=projects/compiler-rt/lib/asan
 ASAN_TESTS_PATH=$ASAN_PATH/tests
-cd llvm_build64/$ASAN_TESTS_PATH
-make -j$MAKE_JOBS AsanTest
-cd $ROOT
-cd llvm_build32/$ASAN_TESTS_PATH
-make -j$MAKE_JOBS AsanTest
-cd $ROOT
-
 ASAN_TEST_BINARY=$ASAN_TESTS_PATH/$BUILD_TYPE/AsanTest
-echo @@@BUILD_STEP run 64-bit asan test@@@
+
+echo @@@BUILD_STEP run 64-bit asan tests@@@
+(cd llvm_build64 && make -j$MAKE_JOBS check-asan)
+# Run unit test binary in a single shard.
 ./llvm_build64/$ASAN_TEST_BINARY
 
-echo @@@BUILD_STEP run 32-bit asan test@@@
+echo @@@BUILD_STEP run 32-bit asan tests@@@
+(cd llvm_build32 && make -j$MAKE_JOBS check-asan)
+# Run unit test binary in a single shard.
 ./llvm_build32/$ASAN_TEST_BINARY
-
-echo @@@BUILD_STEP run 64-bit asan lit tests@@@
-cd llvm_build64
-make -j$MAKE_JOBS check-asan
-cd $ROOT
-
-echo @@@BUILD_STEP run 32-bit asan lit tests@@@
-cd llvm_build32
-make -j$MAKE_JOBS check-asan
-cd $ROOT
 
 if [ "$PLATFORM" == "Darwin" ]; then
 echo @@@BUILD_STEP build asan dynamic runtime@@@
 # Building a fat binary for both 32 and 64 bits.
-cd llvm_build64/$ASAN_PATH
-make -j$MAKE_JOBS clang_rt.asan_osx_dynamic
-cd $ROOT
+(cd llvm_build64/$ASAN_PATH && make -j$MAKE_JOBS clang_rt.asan_osx_dynamic)
 fi
+
+SANITIZER_COMMON_PATH=projects/compiler-rt/lib/sanitizer_common
+SANITIZER_COMMON_TESTS=$SANITIZER_COMMON_PATH/tests
+SANITIZER_COMMON_TEST_BINARY=${SANITIZER_COMMON_TESTS}/${BUILD_TYPE}/SanitizerUnitTest
+
+echo @@@BUILD_STEP run 64-bit sanitizer tests@@@
+(cd llvm_build64 && make -j$MAKE_JOBS check-sanitizer)
+# Run unit test binary in a single shard.
+./llvm_build64/${SANITIZER_COMMON_TEST_BINARY}
+
+echo @@@BUILD_STEP run 32-bit sanitizer tests@@@
+(cd llvm_build32 && make -j$MAKE_JOBS check-sanitizer)
+# Run unit test binary in a single shard.
+./llvm_build32/${SANITIZER_COMMON_TEST_BINARY}
 
