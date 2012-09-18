76052e61e6bde29e3e76a75a75c3b4c163ca8492
Evgeniy Stepanov <eugenis@users.noreply.github.com>
2012-09-18 18:04:15 +0400

    Add msandr-debug.sh

---
+++ llvm/projects/compiler-rt/lib/msan/msandr-debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ulimit -s 8192
+
+HERE=$(dirname $0)
+MSANDR_PATH=$HERE/../../../../../msandr
+DRRUN=$MSANDR_PATH/dr/build/bin64/drrun
+MSANDR=$MSANDR_PATH/build/libmsandr.so
+export LD_USE_LOAD_BIAS=1
+#echo ================= "$@" =================
+$DRRUN -v -debug -client $MSANDR 0 "" -- "$@"
