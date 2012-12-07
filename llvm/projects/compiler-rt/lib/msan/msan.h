cfa7f68e0ac778cd6b4861a8161080d76a770a7c
Evgeniy Stepanov <eugenis@users.noreply.github.com>
2012-12-07 14:29:47 +0400

    Another license header.

--- llvm/projects/compiler-rt/lib/msan/msan.h
+++ llvm/projects/compiler-rt/lib/msan/msan.h
@@ -1,3 +1,17 @@
+//===-- msan.h ------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of MemorySanitizer.
+//
+// Private MSan header.
+//===----------------------------------------------------------------------===//
+
 #ifndef MSAN_H
 #define MSAN_H
 
