33bf8647f5c8bf894e70f1da836ea515d5134b6c
Evgeniy Stepanov <eugenis@users.noreply.github.com>
2012-12-07 17:02:57 +0400

    Avoid including <new>

--- llvm/projects/compiler-rt/lib/msan/msan_new_delete.cc
+++ llvm/projects/compiler-rt/lib/msan/msan_new_delete.cc
@@ -15,34 +15,36 @@
 #include "msan.h"
 
 #include <stddef.h>
-#include <new>
 
 namespace __msan {
 // This function is a no-op. We need it to make sure that object file
-// with our replacements will actually be loaded from static ASan
+// with our replacements will actually be loaded from static MSan
 // run-time library at link-time.
 void ReplaceOperatorsNewAndDelete() { }
 }
 
 using namespace __msan;  // NOLINT
 
+// Fake std::nothrow_t to avoid including <new>.
+namespace std {
+  struct nothrow_t {};
+}  // namespace std
+
+
 #define OPERATOR_NEW_BODY \
   GET_MALLOC_STACK_TRACE; \
   return MsanReallocate(&stack, 0, size, sizeof(u64), false)
 
-void *operator new(size_t size) throw(std::bad_alloc) { OPERATOR_NEW_BODY; }
-void *operator new[](size_t size) throw(std::bad_alloc) { OPERATOR_NEW_BODY; }
-void *operator new(size_t size, std::nothrow_t const&) throw()
-{ OPERATOR_NEW_BODY; }
-void *operator new[](size_t size, std::nothrow_t const&) throw()
-{ OPERATOR_NEW_BODY; }
+void *operator new(size_t size) { OPERATOR_NEW_BODY; }
+void *operator new[](size_t size) { OPERATOR_NEW_BODY; }
+void *operator new(size_t size, std::nothrow_t const&) { OPERATOR_NEW_BODY; }
+void *operator new[](size_t size, std::nothrow_t const&) { OPERATOR_NEW_BODY; }
 
 #define OPERATOR_DELETE_BODY \
   if (ptr) MsanDeallocate(ptr)
 
-void operator delete(void *ptr) throw() { OPERATOR_DELETE_BODY; }
-void operator delete[](void *ptr) throw() { OPERATOR_DELETE_BODY; }
-void operator delete(void *ptr, std::nothrow_t const&) throw()
-{ OPERATOR_DELETE_BODY; }
-void operator delete[](void *ptr, std::nothrow_t const&) throw()
+void operator delete(void *ptr) { OPERATOR_DELETE_BODY; }
+void operator delete[](void *ptr) { OPERATOR_DELETE_BODY; }
+void operator delete(void *ptr, std::nothrow_t const&) { OPERATOR_DELETE_BODY; }
+void operator delete[](void *ptr, std::nothrow_t const&)
 { OPERATOR_DELETE_BODY; }
