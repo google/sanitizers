1bb91308bc4ae88b7ade356cf5ec0c6bb9a4ae8a
timurrrr <timurrrr@google.com>
2013-01-09 12:00:42 +0000

    Fix the bitfield test

--- win/tests/bitfield_uaf_crash.cpp
+++ win/tests/bitfield_uaf_crash.cpp
@@ -33,7 +33,7 @@ int main(void) {
 // CHECK-NOT: This code should be unreachable
 
 // CHECK: AddressSanitizer: heap-use-after-free on address [[ADDR:0x[0-9a-f]+]]
-// CHECK: READ of size 4 at [[ADDR]]
+// CHECK: READ of size {{[124]}} at [[ADDR]]
 // CHECK:   #0 {{.*}} main
 // CHECK: [[ADDR]] is located 0 bytes inside of 4-byte region
 // CHECK: freed by thread T0 here:
