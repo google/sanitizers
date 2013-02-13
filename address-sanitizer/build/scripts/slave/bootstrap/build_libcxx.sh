3807fb0267654cdb1c616b95d3ecaa397868053a
eugenis <eugenis@google.com>
2013-02-13 12:21:27 +0000

    bootstrap++

---
+++ address-sanitizer/build/scripts/slave/bootstrap/build_libcxx.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -e
+
+ARG=$1
+shift
+
+if [ "z$ARG" == "z--msan" ]; then
+  CFLAGS="-fsanitize=memory -fsanitize-memory-track-origins"
+  LDFLAGS="-fsanitize=memory -pie"
+  ARG=$1
+  shift
+fi
+
+if [ "z$ARG" == "z" ]; then
+  echo "Usage: $0 [--msan] llvmsrcpath"
+  exit 1
+fi
+
+LLVM=$ARG
+
+if [ "z$LLVM_BIN" == "z" ]; then
+  echo "Please set \$LLVM_BIN to the location of msan-enabled 'clang' binary."
+  exit 1
+fi
+
+CLANG=$LLVM_BIN/clang
+CLANGXX=$LLVM_BIN/clang++
+
+LIBCXXABI=$LLVM/projects/libcxxabi
+LIBCXX=$LLVM/projects/libcxx
+
+# Build libcxxabi.
+
+LIBCXXABI_CFLAGS="\
+-g -O3 -fPIC \
+-std=c++0x \
+-fstrict-aliasing \
+-Wstrict-aliasing=2 -Wsign-conversion -Wshadow -Wconversion -Wunused-variable \
+-Wmissing-field-initializers -Wchar-subscripts -Wmismatched-tags \
+-Wmissing-braces -Wshorten-64-to-32 -Wsign-compare -Wstrict-aliasing=2 \
+-Wstrict-overflow=4 -Wunused-parameter -Wnewline-eof \
+-I${LIBCXXABI}/include \
+-I${LIBCXX}/include \
+-I/usr/lib/gcc/x86_64-linux-gnu/4.6/include \
+$CFLAGS \
+"
+
+LIBCXXABI_OBJS=
+for source in ${LIBCXXABI}/src/*.cpp; do
+    obj=$(basename ${source%.cpp}.o)
+    echo "CXX $obj"
+    $CLANGXX -c $LIBCXXABI_CFLAGS $source
+    LIBCXXABI_OBJS="$OBJS $obj"
+done
+
+LIBCXXABI_LDFLAGS="\
+-o libc++abi.so.1.0 \
+-shared \
+-nodefaultlibs \
+-Wl,-soname,libc++abi.so.1 \
+-lpthread \
+-lrt \
+-lc \
+$LDFLAGS \
+"
+
+echo "SOLINK libc++abi.so.1.0"
+
+$CLANG $LIBCXXABI_OBJS $LIBCXXABI_LDFLAGS
+
+echo "SYMLINKS"
+
+ln -sf libc++abi.so.1.0 libc++abi.so.1
+ln -sf libc++abi.so.1 libc++abi.so
+
+# Now build libcxx.
+
+LIBCXX_CFLAGS="\
+-g -Os -fPIC \
+-std=c++0x \
+-fstrict-aliasing \
+-Wall -Wextra -Wshadow -Wconversion -Wnewline-eof -Wpadded \
+-Wmissing-prototypes -Wstrict-aliasing=2 -Wstrict-overflow=4 \
+-nostdinc++ \
+-I${LIBCXXABI}/include \
+-I${LIBCXX}/include \
+$CFLAGS \
+"
+
+LIBCXX_OBJS=
+for source in ${LIBCXX}/src/*.cpp; do
+    obj=$(basename ${source%.cpp}.o)
+    echo "CXX $obj"
+    $CLANGXX -c $LIBCXX_CFLAGS $source
+    LIBCXX_OBJS="$OBJS $obj"
+done
+
+LIBCXX_LDFLAGS="\
+-o libc++.so.1.0 \
+-shared \
+-nodefaultlibs \
+-Wl,-soname,libc++.so.1 \
+-lpthread \
+-lrt \
+-lc \
+$LDFLAGS \
+"
+
+echo "SOLINK libc++.so.1.0"
+
+$CLANG $LIBCXX_OBJS $LIBCXX_LDFLAGS
+
+ln -sf libc++.so.1.0 libc++.so.1
+ln -sf libc++.so.1 libc++.so
+
+echo "SUCCESS"
--- address-sanitizer/build/scripts/slave/buildbot_bootstrap.sh
+++ address-sanitizer/build/scripts/slave/buildbot_bootstrap.sh
@@ -15,11 +15,12 @@ if [ "$BUILDBOT_CLOBBER" != "" ]; then
   echo @@@BUILD_STEP clobber@@@
   rm -rf llvm
   rm -rf llvm_build0
+  rm -rf libcxx_build_msan
   rm -rf llvm_build_msan
 fi
 
 MAKE_JOBS=${MAX_MAKE_JOBS:-16}
-LLVM_CHECKOUT=$ROOT/llvm
+LLVM=$ROOT/llvm
 
 CMAKE_COMMON_OPTIONS="-GNinja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON"
 CMAKE_STAGE1_OPTIONS="${CMAKE_COMMON_OPTIONS}"
@@ -32,8 +33,15 @@ echo @@@BUILD_STEP build stage1 clang@@@
 if [ ! -d llvm_build0 ]; then
   mkdir llvm_build0
 fi
-(cd llvm_build0 && cmake ${CMAKE_OPTIONS} $LLVM_CHECKOUT && ninja) || \
+(cd llvm_build0 && cmake ${CMAKE_STAGE1_OPTIONS} $LLVM_CHECKOUT && ninja) || \
   echo @@@STEP_FAILURE@@@
 
+echo @@@BUILD_STEP build libcxxabi/msan@@@
+
+CLANG_PATH=llvm_build0/bin
 CMAKE_STAGE2_COMMON_OPTIONS="${CMAKE_COMMON_OPTIONS} -DLLVM_ENABLE_WERROR=ON"
 CMAKE_MSAN_OPTIONS="${CMAKE_STAGE2_COMMON_OPTIONS} -DCMAKE_C_COMPILER=${CLANG_PATH}/clang -DCMAKE_CXX_COMPILER=${CLANG_PATH}/clang++"
+
+(cd libcxx_build_msan &&
+    LLVM_BIN=$CLANG_PATH $HERE/bootstrap/build_libcxx.sh --msan $LLVM) ||
+echo @@@STEP_FAILURE@@@
