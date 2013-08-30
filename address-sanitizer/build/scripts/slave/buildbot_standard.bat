f069fccf0ce254e97a20e0ade7c4146139e322d5
timurrrr <timurrrr@3c3640eb-14a6-2bb8-0b94-e9e73afe33f5>
2013-08-30 14:02:27 +0000

    Revert the recent Win command changes, will hopefully fix the bot

--- address-sanitizer/build/scripts/slave/buildbot_standard.bat
+++ address-sanitizer/build/scripts/slave/buildbot_standard.bat
@@ -18,15 +18,25 @@ if NOT "%BUILDBOT_REVISION%" == "" set REV_ARG="-r%BUILDBOT_REVISION%"
 :: call -> because "svn" might be a batch script, ouch
 call svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm %REV_ARG% || goto :DIE
 call svn co http://llvm.org/svn/llvm-project/cfe/trunk llvm/tools/clang %REV_ARG% || goto :DIE
-call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk llvm/projects/compiler-rt %REV_ARG% || goto :DIE
+call svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt %REV_ARG% || goto :DIE
 call svn co http://address-sanitizer.googlecode.com/svn/trunk/win/tests win_tests || echo "Failed to update Windows tests, alas..."
 
 set ROOT=%cd%
 
+echo @@@BUILD_STEP cmake llvm@@@
+mkdir llvm-build
+:: TODO(timurrrr): Is this enough to force a full re-configure?
+del llvm-build\CMakeCache.txt
+cd llvm-build
+cmake -DLLVM_TARGETS_TO_BUILD=X86 ..\llvm || goto :DIE
+echo @@@BUILD_STEP build llvm@@@
+cmake --build . || goto :DIE
+cd %ROOT%
+
+:: TODO(timurrrr) echo @@@BUILD_STEP test llvm@@@
+
 echo @@@BUILD_STEP build asan RTL@@@
-:: TODO(timurrrr): Only needed to build asan_thunk.dll actually.
-:: Still useful to detect build errors early...
-set ASAN_PATH=llvm\projects\compiler-rt\lib\asan
+set ASAN_PATH=compiler-rt\lib\asan
 cd %ASAN_PATH% || goto :DIE
 :: This only compiles, not links.
 del *.pdb *.obj *.lib || goto :DIE
@@ -42,28 +52,15 @@ cl /nologo /WX /W3 /MT /Zi /DASAN_DLL_THUNK /c asan_dll_thunk.cc || goto :DIE
 lib /nologo /OUT:asan_dll_thunk.lib asan_dll_thunk.obj
 cd %ROOT%
 
-echo @@@BUILD_STEP cmake llvm@@@
-mkdir llvm-build
-:: TODO(timurrrr): Is this enough to force a full re-configure?
-del llvm-build\CMakeCache.txt
-cd llvm-build
-cmake -DLLVM_TARGETS_TO_BUILD=X86 ..\llvm || goto :DIE
-echo @@@BUILD_STEP build llvm@@@
-cmake --build . || goto :DIE
-cd %ROOT%
-
-:: TODO(timurrrr) echo @@@BUILD_STEP test llvm@@@
-
 echo @@@BUILD_STEP asan test@@@
 cd win_tests || goto :DIE
 C:\cygwin\bin\make -s PLATFORM=Windows RM_F="/cygdrive/c/cygwin/bin/rm -f" clean || goto :DIE
-C:\cygwin\bin\make -s PLATFORM=Windows CC=../llvm-build/bin/Debug/clang-cl FILECHECK=../llvm-build/bin/Debug/FileCheck CFLAGS="-fsanitize=address" -k || goto :DIE
+C:\cygwin\bin\make -s PLATFORM=Windows CC=../llvm-build/bin/Debug/clang-cl FILECHECK=../llvm-build/bin/Debug/FileCheck CFLAGS="-Xclang -fsanitize=address" EXTRA_OBJ=../compiler-rt/lib/asan/asan_rtl.lib -k || goto :DIE
 
 echo @@@BUILD_STEP asan DLL thunk test@@@
 cd dll_tests || goto :DIE
 C:\cygwin\bin\make -s PLATFORM=Windows RM_F="/cygdrive/c/cygwin/bin/rm -f" clean || goto :DIE
-:: TODO(timurrrr): clang-cl should automatically link the needed parts of the RTL.
-C:\cygwin\bin\make -s PLATFORM=Windows CC=../../llvm-build/bin/Debug/clang-cl FILECHECK=../../llvm-build/bin/Debug/FileCheck CFLAGS="-fsanitize=address" EXTRA_HOST_LIBS=../../llvm/projects/compiler-rt/lib/asan/asan_rtl.lib EXTRA_GUEST_LIBS="../../llvm/projects/compiler-rt/lib/asan/asan_dll_thunk.lib" -k || goto :DIE
+C:\cygwin\bin\make -s PLATFORM=Windows CC=../../llvm-build/bin/Debug/clang-cl FILECHECK=../../llvm-build/bin/Debug/FileCheck CFLAGS="-Xclang -fsanitize=address" EXTRA_HOST_LIBS=../../compiler-rt/lib/asan/asan_rtl.lib EXTRA_GUEST_LIBS="../../compiler-rt/lib/asan/asan_dll_thunk.lib" -k || goto :DIE
 cd %ROOT%
 
 :: TODO(timurrrr) echo @@@BUILD_STEP asan test64@@@
