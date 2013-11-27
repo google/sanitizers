8395dcc63dcc990e18ffbc7278cad4c2bae9e62d
timurrrr <timurrrr@google.com>
2013-11-27 14:41:20 +0000

    Add a test that requires a DLL thunk for __asan_handle_no_return

---
+++ win/tests/dll_tests/noreturn_crash.cpp
@@ -0,0 +1,42 @@
+/* Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This file is a part of AddressSanitizer, an address sanity checker.
+
+#include "../common.h"
+
+void noreturn_f() {
+  volatile int subscript = -1;
+  volatile char buffer[42];
+  buffer[subscript] = 42;
+  _exit(1);
+}
+
+DLLEXPORT int test_function() {
+  noreturn_f();
+
+  UNREACHABLE();
+// CHECK-NOT: This code should be unreachable
+
+// CHECK: AddressSanitizer: stack-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
+// CHECK: WRITE of size 1 at [[ADDR]] thread T0
+// CHECK:   noreturn_f
+// CHECK:   test_function
+// CHECK:   main
+// CHECK: Address [[ADDR]] is located in stack of thread T0 at offset {{.*}} in frame
+// CHECK:   noreturn_f
+// CHECK: Shadow bytes around the buggy address
+  return 0;
+}
