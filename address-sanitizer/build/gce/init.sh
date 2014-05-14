363585d8295fd2e31ca880731ea7ad2bc7e50c7c
konstantin.s.serebryany <konstantin.s.serebryany@gmail.com>
2014-05-14 08:07:49 +0000

    typo

--- address-sanitizer/build/gce/init.sh
+++ address-sanitizer/build/gce/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get --yes install subversion cmake build-essential git
-midir -p bin
+mkdir -p bin
 svn checkout http://address-sanitizer.googlecode.com/svn/trunk/build/gce scripts
 scripts/install_ninja.sh
