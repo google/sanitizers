4e14be52ad6d20da96fd2620da66f50ec3fcd9eb
preobr <preobr@google.com>
2014-07-04 09:18:18 +0000

    Add vagrant_kasan

---
+++ address-sanitizer/vagrant_kasan/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -o pipefail
+set -eux
+
+function get_gcc() {
+  svn checkout -r 203101 svn://gcc.gnu.org/svn/gcc/trunk gcc_kasan
+  # wget https://address-sanitizer.googlecode.com/files/gcc-r203101-kasan.patch
+
+  cd gcc_kasan
+  patch -p0 -i /vagrant/gcc-r203101-kasan.patch
+}
+
+function make_gcc() {
+  mkdir gcc_build
+  cd gcc_build
+
+  ../gcc_kasan/configure --disable-multilib --disable-bootstrap --enable-languages=c,c++ --prefix=/home/vagrant/gcc_install/
+
+  make -j8
+  make install
+}
+
+function get_kasan() {
+  git clone https://github.com/google/kasan.git
+}
+
+function make_kasan() {
+  cd kasan
+
+  cp /vagrant/kernel_config .config
+  make oldconfig
+
+  make -j8 deb-pkg LOCALVERSION=-asan CC=~/gcc_install/bin/gcc
+}
+
+function main() {
+  case "$1" in
+    get_gcc|make_gcc|get_kasan|make_kasan)
+      ${1}
+      ;;
+    *)
+      echo "Usage: $0 {get_gcc|make_gcc|get_kasan|make_kasan}"
+      exit 1
+esac
+}
+
+main $@ ""
