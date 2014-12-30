496bcc8e2fd7f605ed4fdf0571b17ba44969206b
samsonov <samsonov@google.com>
2014-12-30 19:57:41 +0000

    Kill bootstrap build scripts that are either obsolete or moved to zorg

--- address-sanitizer/build/scripts/slave/buildbot_selector.py
+++ address-sanitizer/build/scripts/slave/buildbot_selector.py
@@ -16,29 +16,21 @@ def cmd_call(path):
 BOT_ASSIGNMENT = {
     'win': cmd_call('buildbot_standard.bat'),
     'linux': bash('buildbot_standard.sh'),
-    'linux-bootstrap': bash('buildbot_bootstrap.sh'),
     'linux-chrome-asan': bash('buildbot_chrome_asan.sh'),
     'linux-chrome-tsan': bash('buildbot_chrome_tsan.sh'),
     'linux-perf-asan': bash('buildbot_perf_asan.sh'),
     'mac10.9-cmake': bash('buildbot_cmake.sh'),
     'mac10.9': bash('buildbot_standard.sh'),
-    'sanitizer-ppc64-linux1': bash('buildbot_cmake.sh'),
-    'sanitizer-x86_64-linux': bash('buildbot_cmake.sh'),
-    'sanitizer-x86_64-linux-bootstrap': bash('buildbot_bootstrap.sh'),
 }
 
 BOT_ADDITIONAL_ENV = {
     'win': {},
     'linux': { 'CHECK_TSAN': '1', 'BUILD_ASAN_ANDROID' : '1' },
-    'linux-bootstrap': {},
     'linux-chrome-asan': {},
     'linux-chrome-tsan': {},
     'linux-perf-asan': {},
     'mac10.9-cmake': { 'MAX_MAKE_JOBS': '8' },
     'mac10.9': { 'MAX_MAKE_JOBS': '8' },
-    'sanitizer-ppc64-linux1': { 'HAVE_NINJA': '0' },
-    'sanitizer-x86_64-linux': { 'BUILD_ANDROID' : '1', 'RUN_ANDROID' : '1', 'HAVE_NINJA' : '1' },
-    'sanitizer-x86_64-linux-bootstrap': {},
 }
 
 def Main():
