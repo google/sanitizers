305e5498dc17eff4449c4e21f24d90ed65d7bc64
earthdok <earthdok@google.com>
2015-02-05 20:57:20 +0000

    [glibc bot] Basic builder.

--- address-sanitizer/glibc_buildbot/scripts/slave/glibc-x86_64-linux.sh
+++ address-sanitizer/glibc_buildbot/scripts/slave/glibc-x86_64-linux.sh
@@ -4,4 +4,34 @@ set -x
 set -e
 set -u
 
-echo @@@BUILD_STEP test@@@
+echo @@@BUILD_STEP sync@@@
+
+root_dir=$(pwd)
+src_dir="${root}/glibc"
+build_dir="${root}/build"
+
+if [ -d ${src_dir} ]; then
+  git clone git://sourceware.org/git/glibc.git ${src_dir}
+else
+  cd ${src_dir}
+  git pull
+  cd ${root}
+fi
+
+
+echo @@@BUILD_STEP configure@@@
+
+mkdir -p $build_dir
+cd $build_dir
+${src_dir}/configure --prefix=/usr --enable-add-ons
+
+num_jobs=$(getconf _NPROCESSORS_ONLN)
+
+
+echo @@@BUILD_STEP make@@@
+
+make -j${num_jobs} -k
+
+echo @@@BUILD_STEP check@@@
+
+make -j${num_jobs} -k check
