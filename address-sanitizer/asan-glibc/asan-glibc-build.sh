113b5178723b49456979b198ea7b160c867305d1
earthdok <earthdok@google.com>
2015-03-27 15:21:41 +0000

    Update asan-glibc build scripts.
    Compensate for the fact that GCC has an older version of ASan which is
    incompatible with trunk clang.

--- address-sanitizer/asan-glibc/asan-glibc-build.sh
+++ address-sanitizer/asan-glibc/asan-glibc-build.sh
@@ -5,6 +5,9 @@ GLIBC=$HERE/glibc-2.19
 PLAIN_BUILD=$HERE/plain-build
 ASAN_BUILD=$HERE/asan-build
 ASAN_INST=$HERE/asan-inst
+PATH=$HOME/toolchains/gcc-trunk/bin:$PATH
+
+set -xe
 
 get_glibc() {
  wget http://ftp.gnu.org/gnu/glibc/glibc-$1.tar.bz2
@@ -20,28 +23,29 @@ build_plain() {
 }
 
 build_asan() {
+  # Temporary, see comment in asan-init-stub.c
+  gcc -c asan-init-stub.c -o asan-init-stub.o -fPIC
   rm -rf $ASAN_BUILD
   mkdir $ASAN_BUILD
   cd $ASAN_BUILD
-  CC=$HERE/asan-glibc-gcc-wrapper.py $GLIBC/configure --prefix=$ASAN_INST && \
+  CC=$HERE/asan-glibc-gcc-wrapper.py $GLIBC/configure --prefix=/usr/ && \
     make -j $J  -C $GLIBC objdir=`pwd` lib > mk
   cp -v libc.so $ASAN_INST/lib/libc-*.so
   cd $HERE
 }
 
 test_asan() {
-  LD_SO=$(echo ${ASAN_INST}/lib/ld-2.*.so)
-  echo $LD_SO
   clang -c asan-glibc-test.c
   clang asan-glibc-test.o  \
-    -Wl,-rpath=${ASAN_INST}/lib  -Wl,-dynamic-linker=$LD_SO \
+    -Wl,-rpath=${ASAN_INST}/lib \
     -fsanitize=address -o asan_glibc_test
    export ASAN_OPTIONS=detect_odr_violation=0
-   ./asan_glibc_test           2>&1 | grep strsep
-   ./asan_glibc_test  1        2>&1 | grep strverscmp
-   ./asan_glibc_test  1 2      2>&1 | grep getenv
-   ./asan_glibc_test  1 2 3    2>&1 | grep nss_hostname_digits_dots
-   ./asan_glibc_test  1 2 3 4  2>&1 | grep internal_fnmatch
+   ./asan_glibc_test           2>&1 | grep strsep || echo FAIL
+   ./asan_glibc_test  1        2>&1 | grep strverscmp  || echo FAIL
+   ./asan_glibc_test  1 2      2>&1 | grep getenv || echo FAIL
+   ./asan_glibc_test  1 2 3    2>&1 | grep nss_hostname_digits_dots || echo FAIL
+   ./asan_glibc_test  1 2 3 4  2>&1 | grep internal_fnmatch || echo FAIL
+
 }
 
  get_glibc 2.19
--- address-sanitizer/asan-glibc/asan-glibc-gcc-wrapper.py
+++ address-sanitizer/asan-glibc/asan-glibc-gcc-wrapper.py
@@ -10,7 +10,8 @@ blacklist=["rtld", "/dl-", "elf/", "string/mem", "time/time",
 #0 0x7f5147637bd9 in _mm_load_si128 .../x86_64-unknown-linux-gnu/5.0.0/include/emmintrin.h:688
 #1 0x7f5147637bd9 in __strcspn_sse42 .../glibc-2.19/string/../sysdeps/x86_64/multiarch/strcspn-c.c:123
            "string/strcspn-c",  # May read 16-aligned data outside of buffer.
-           "string/strpbrk-c",  # Same function asn strcspn
+           "string/strpbrk-c",  # Same function as strcspn
+           "string/strspn-c",  # Same.
            ]
 
 def AllowAsan(obj):
@@ -36,6 +37,11 @@ if __name__ == '__main__':
     last_was_minus_o = a == "-o"
   if asan_ok:
     print >> sys.stderr, "ASAN:", a
-    res.append("-fsanitize=address") 
-  # print res
+    res.append("-fsanitize=address")
+    # Temporarily disable UAR. See comment asan-init-stub.c
+    res.append("--param")
+    res.append("asan-use-after-return=0")
+  if "libc.so.6" in ' '.join(res):
+    res.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'asan-init-stub.o'))
   os.execvp(GCC, res)
