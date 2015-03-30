2a13c9596cc87b8eb03d32b79ae84b418ee666f2
earthdok <earthdok@google.com>
2015-03-30 20:40:14 +0000

    Update glibc build scripts.

--- address-sanitizer/asan-glibc/asan-glibc-build.sh
+++ address-sanitizer/asan-glibc/asan-glibc-build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 HERE=$(pwd)
-J=32
+J=${J:-32}
 GLIBC=$HERE/glibc-2.19
 PLAIN_BUILD=$HERE/plain-build
 ASAN_BUILD=$HERE/asan-build
 ASAN_INST=$HERE/asan-inst
+ASAN_LIBS=$HERE/asan-libs
 PATH=$HOME/toolchains/gcc-trunk/bin:$PATH
 
-set -xe
+# Debian
+configure_flags="--prefix=/usr --without-cvs --enable-add-ons=libidn,nptl --enable-profile --without-selinux --enable-stackguard-randomization --enable-obsolete-rpc"
+
+# Do not warn about --prefix=/usr
+configure_flags="$configure_flags --disable-sanity-checks"
+
+set -xeu
 
 get_glibc() {
  wget http://ftp.gnu.org/gnu/glibc/glibc-$1.tar.bz2
@@ -18,33 +25,37 @@ build_plain() {
   rm -rf $PLAIN_BUILD
   mkdir $PLAIN_BUILD
   cd $PLAIN_BUILD
-  $GLIBC/configure --prefix=$ASAN_INST && make -j $J && make install
+
+  $GLIBC/configure $configure_flags
+  make -j $J
+  make install install_root=$ASAN_INST
   cd $HERE
 }
 
 build_asan() {
   # Temporary, see comment in asan-init-stub.c
   gcc -c asan-init-stub.c -o asan-init-stub.o -fPIC
+  
   rm -rf $ASAN_BUILD
-  mkdir $ASAN_BUILD
+  mkdir -p $ASAN_BUILD
   cd $ASAN_BUILD
-  CC=$HERE/asan-glibc-gcc-wrapper.py $GLIBC/configure --prefix=/usr/ && \
-    make -j $J  -C $GLIBC objdir=`pwd` lib > mk
-  cp -v libc.so $ASAN_INST/lib/libc-*.so
+  CC=$HERE/asan-glibc-gcc-wrapper.py $GLIBC/configure $configure_flags
+  make -j $J lib
+  cp -v libc.so $ASAN_INST/lib64/libc-*.so
   cd $HERE
 }
 
 test_asan() {
   clang -c asan-glibc-test.c
   clang asan-glibc-test.o  \
-    -Wl,-rpath=${ASAN_INST}/lib \
+    -Wl,-rpath=${ASAN_INST}/lib64 \
     -fsanitize=address -o asan_glibc_test
    export ASAN_OPTIONS=detect_odr_violation=0
-   ./asan_glibc_test           2>&1 | grep strsep || echo FAIL
-   ./asan_glibc_test  1        2>&1 | grep strverscmp  || echo FAIL
-   ./asan_glibc_test  1 2      2>&1 | grep getenv || echo FAIL
-   ./asan_glibc_test  1 2 3    2>&1 | grep nss_hostname_digits_dots || echo FAIL
-   ./asan_glibc_test  1 2 3 4  2>&1 | grep internal_fnmatch || echo FAIL
+   ./asan_glibc_test           2>&1 | grep strsep || echo 'FAIL'
+   ./asan_glibc_test  1        2>&1 | grep strverscmp  || echo 'FAIL'
+   ./asan_glibc_test  1 2      2>&1 | grep getenv || echo 'FAIL'
+   ./asan_glibc_test  1 2 3    2>&1 | grep nss_hostname_digits_dots || echo 'FAIL'
+   ./asan_glibc_test  1 2 3 4  2>&1 | grep internal_fnmatch || echo 'FAIL'
 
 }
 
--- address-sanitizer/asan-glibc/asan-glibc-gcc-wrapper.py
+++ address-sanitizer/asan-glibc/asan-glibc-gcc-wrapper.py
@@ -3,15 +3,19 @@ import os
 import re
 import sys
 
-blacklist=["rtld", "/dl-", "elf/", "string/mem", "time/time",
-           "time/gettimeofday", "time/timegm", "time/timespec_get",
-           "nptl/libc_pthread_init", "nptl/register-atfork", "string/strstr",
-           "string/strcasestr",
+ASAN_INIT_STUB=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'asan-init-stub.o')
+GCC = 'gcc'
+
+blacklist=['rtld', '/dl-', 'elf/', 'string/mem', 'time/time',
+           'time/gettimeofday', 'time/timegm', 'time/timespec_get',
+           'nptl/libc_pthread_init', 'nptl/register-atfork', 'string/strstr',
+           'string/strcasestr',
 #0 0x7f5147637bd9 in _mm_load_si128 .../x86_64-unknown-linux-gnu/5.0.0/include/emmintrin.h:688
 #1 0x7f5147637bd9 in __strcspn_sse42 .../glibc-2.19/string/../sysdeps/x86_64/multiarch/strcspn-c.c:123
-           "string/strcspn-c",  # May read 16-aligned data outside of buffer.
-           "string/strpbrk-c",  # Same function as strcspn
-           "string/strspn-c",  # Same.
+           'string/strcspn-c',  # May read 16-aligned data outside of buffer.
+           'string/strpbrk-c',  # Same function as strcspn
+           'string/strspn-c',  # Same.
            ]
 
 def AllowAsan(obj):
@@ -19,29 +23,27 @@ def AllowAsan(obj):
     if re.search(b, obj): return False
   return True
 
+def o():
+  try:
+    i = sys.argv.index('-o')
+    return sys.argv[i + 1]
+  except Exception:
+    return ''
+
 if __name__ == '__main__':
-  last_was_minus_o = False
-  asan_ok = False;
-  GCC = "gcc"
-  res = [GCC]
-  for a in sys.argv[1:]:
-    if not re.match(r"-Wl,-z,defs", a):
-      res.append(a)
-    # print last_was_minus_o, a
-    if last_was_minus_o:
-      m = re.match(r"/.*build/(.*).os", a)
-      if m:
-        obj = m.group(1)
-        if AllowAsan(obj):
-          asan_ok = True;
-    last_was_minus_o = a == "-o"
+  args = sys.argv[1:]
+  args = [arg for arg in args if arg != '-Wl,-z,defs']
+  asan_ok = re.match(r'/.*build/(.*).os', o()) and AllowAsan(o())
+
   if asan_ok:
-    print >> sys.stderr, "ASAN:", a
-    res.append("-fsanitize=address")
+    print >> sys.stderr, 'ASAN:', o()
+    args.append('-fsanitize=address')
     # Temporarily disable UAR. See comment asan-init-stub.c
-    res.append("--param")
-    res.append("asan-use-after-return=0")
-  if "libc.so.6" in ' '.join(res):
-    res.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            'asan-init-stub.o'))
-  os.execvp(GCC, res)
+    args.append('--param')
+    args.append('asan-use-after-return=0')
+    if '-c' not in args and '-S' not in args and '-E' not in args:
+      args.append(ASAN_INIT_STUB)
+
+  args.append('-fno-omit-frame-pointer')
+
+  os.execvp(GCC, [GCC] + args)
