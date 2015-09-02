7af8090d1cce999d692fa52014d7f70bfd69127e
Andrey Konovalov <andreyknvl@google.com>
2015-09-02 12:50:08 +0200

    Update kasan symbolizer script
    1. Support prefixes for frames
    2. Support slub objerr traces

--- address-sanitizer/tools/kasan_symbolize.py
+++ address-sanitizer/tools/kasan_symbolize.py
@@ -6,19 +6,22 @@ import sys
 import subprocess
 
 time_re = re.compile(
-  '^(?P<time>\[[ ]*[0-9\.]+\]) ?(?P<suffix>.*)$'
+  '^(?P<time>\[[ ]*[0-9\.]+\]) ?(?P<body>.*)$'
 )
 
 frame_re = re.compile(
-  '^ \[\<(?P<addr>[0-9A-Fa-f]+)\>\] ' + 
-  '(\? )?'                            +
-  '(?P<suffix>'                       +
-    '(?P<function>[^\+]+)'            +
-    '\+'                              +
-    '0x(?P<offset>[0-9A-Fa-f]+)'      +
-    '/'                               +
-    '0x(?P<size>[0-9A-Fa-f]+)'        +
-    '( \[(?P<module>.+)\])?'          +
+  '^'                                  +
+  '(?P<prefix>[^\[\t]*)'               +
+  '(\[\<(?P<addr>[0-9A-Fa-f]+)\>\])?'  +
+  '( |\t)'                             +
+  '(?P<body>'                          +
+    '(\? )?'                           +
+    '(?P<function>[^\+]+)'             +
+    '\+'                               +
+    '0x(?P<offset>[0-9A-Fa-f]+)'       +
+    '/'                                +
+    '0x(?P<size>[0-9A-Fa-f]+)'         +
+    '( \[(?P<module>.+)\])?'           +
   ')$'
 )
 
@@ -94,7 +97,7 @@ class ReportProcesser:
   def StripTime(self, line):
     match = time_re.match(line)
     if match != None:
-      line = match.group('suffix')
+      line = match.group('body')
     return line
 
   def ProcessLine(self, line):
@@ -103,8 +106,10 @@ class ReportProcesser:
       print line
       return
 
+    prefix = match.group('prefix')
+
     addr = match.group('addr')
-    suffix = match.group('suffix')
+    body = match.group('body')
 
     function = match.group('function')
     offset = match.group('offset')
@@ -140,8 +145,8 @@ class ReportProcesser:
       return
 
     for frame in frames[:-1]:
-      self.PrintInlinedFrame(addr, frame[0], frame[1], suffix)
-    self.PrintFrame(addr, frames[-1][0], frames[-1][1], suffix)
+      self.PrintInlinedFrame(prefix, addr, frame[0], frame[1], body)
+    self.PrintFrame(prefix, addr, frames[-1][0], frames[-1][1], body)
 
   def LoadModule(self, module):
     if module in self.module_symbolizers.keys():
@@ -155,20 +160,22 @@ class ReportProcesser:
     self.module_offset_loaders[module] = SymbolOffsetLoader(module_path)
     return True
 
-  def PrintFrame(self, addr, func, fileline, suffix):
+  def PrintFrame(self, prefix, addr, func, fileline, body):
     if self.strip_path != None:
       fileline_parts = fileline.split(self.strip_path, 1)
       if len(fileline_parts) >= 2:
         fileline = fileline_parts[1].lstrip('/')
-    print ' [<%s>] %s %s' % (addr, suffix, fileline)
+    if addr == None:
+        addr = '      none      ';
+    print '%s[<%s>] %s %s' % (prefix, addr, body, fileline)
 
-  def PrintInlinedFrame(self, addr, func, fileline, suffix):
+  def PrintInlinedFrame(self, prefix, addr, func, fileline, body):
     if self.strip_path != None:
       fileline_parts = fileline.split(self.strip_path, 1)
       if len(fileline_parts) >= 2:
         fileline = fileline_parts[1].lstrip('/')
     addr = '     inlined    ';
-    print ' [<%s>] %s %s %s' % (addr, suffix, func, fileline) 
+    print '%s[<%s>] %s %s %s' % (prefix, addr, body, func, fileline)
 
   def Finalize(self):
     for module, symbolizer in self.module_symbolizers.items():
