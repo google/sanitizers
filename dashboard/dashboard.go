ce6547be79ab02a3e5c5876e9599530c2b998ec8
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-01-08 14:03:49 -0800

    added chromium libfuzzer build.

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -88,11 +88,13 @@ func main() {
 		{"sanitizer-x86_64-linux-bootstrap", "http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-bootstrap"},
 		{"sanitizer-x86_64-linux-fast", "http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-fast"},
 		{"sanitizer-x86_64-linux-autoconf", "http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-autoconf"},
-		{"sanitizer-x86_64-linux-fuzzer", "http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-fuzzer"},
 		{"sanitizer-ppc64-linux1", "http://lab.llvm.org:8011/builders/sanitizer-ppc64-linux1"},
 		{"sanitizer-ppc64le-linux", "http://lab.llvm.org:8011/builders/sanitizer-ppc64le-linux"},
 		{"clang-cmake-armv7-a15-full", "http://lab.llvm.org:8011/builders/clang-cmake-armv7-a15-full"},
 		{"clang-cmake-thumbv7-a15-full-sh", "http://lab.llvm.org:8011/builders/clang-cmake-thumbv7-a15-full-sh"},
+		{"LibFuzzer", ""},
+		{"sanitizer-x86_64-linux-fuzzer", "http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-fuzzer"},
+		{"chromium-x86_64-linux-fuzzer", "https://build.chromium.org/p/chromium.fyi/builders/Libfuzzer%20Upload%20Linux"},
 	}
 
 	statuses := make([]status, len(bots))
