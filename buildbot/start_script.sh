d2f44a23ef5b3059d700f121f5bcef750f52998e
Vitaly Buka <vitalybuka@google.com>
2017-10-14 15:36:42 -0700

    Startup script for GCE instances

---
+++ buildbot/start_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+BOT_DIR=/b
+BOT_NAME=$1
+BOT_PASS=$2
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y \
+ buildbot-slave \
+ subversion \
+ g++ \
+ cmake \
+ binutils-gold \
+ binutils-dev \
+ ninja-build
+
+update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20
+update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
+
+mkdir -p $BOT_DIR
+chown buildbot:buildbot $BOT_DIR
+
+buildslave create-slave $BOT_DIR lab.llvm.org:9990 $BOT_NAME $BOT_PASS
+
+echo "Vitaly Buka <vitalybuka@google.com>" > $BOT_DIR/info/admin
+
+uname -a | head -n1 > $BOT_DIR/info/host
+cmake --version | head -n1 >> $BOT_DIR/info/host
+g++ --version | head -n1 >> $BOT_DIR/info/host
+ld --version | head -n1 >> $BOT_DIR/info/host
+date >> $BOT_DIR/info/host
+
+echo "SLAVE_RUNNER=/usr/bin/buildslave
+SLAVE_ENABLED[1]=\"1\"
+SLAVE_NAME[1]=\"buildslave1\"
+SLAVE_USER[1]=\"buildbot\"
+SLAVE_BASEDIR[1]=\"$BOT_DIR\"
+SLAVE_OPTIONS[1]=\"\"
+SLAVE_PREFIXCMD[1]=\"\"" > /etc/default/buildslave
+
+chown -R buildbot:buildbot $BOT_DIR
+service buildslave restart
