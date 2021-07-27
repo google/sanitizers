d54f8798d922e68670daa34d31fe9ab7c23da4d9
Vitaly Buka <vitalybuka@google.com>
2021-07-27 12:51:28 -0700

    Shuffle workers order

--- buildbot/start_script.sh
+++ buildbot/start_script.sh
@@ -108,7 +108,7 @@ function claim_worker() {
 while true ; do
   sleep 30
   (
-    for W in 1 3 7 2 4 8 ; do
+    for W in $(echo "1 3 7 2 4 8" | tr ' ' '\n' | shuf) ; do
       claim_worker "sanitizer-buildbot${W}" && exit
     done
     # No unclaimed workers?
