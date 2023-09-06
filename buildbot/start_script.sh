c7fb1cd0a0ddbdcc082c3217f2a24c9da8646be5
Vitaly Buka <vitalybuka@google.com>
2023-09-05 19:29:22 -0700

    Disable agent
    Attempt to intestate connection issues.

--- buildbot/start_script.sh
+++ buildbot/start_script.sh
@@ -34,8 +34,8 @@ mountpoint /tmp     || mount -o nosuid -t tmpfs tmpfs /tmp || $ON_ERROR
 ${SCRIPT_DIR}/install_deps.sh
 
 # Optional, ingore if it fails.
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-bash add-google-cloud-ops-agent-repo.sh --also-install
+# curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+# bash add-google-cloud-ops-agent-repo.sh --also-install
 
 mkdir -p $BOT_DIR
 if lsblk /dev/nvme0n2 ; then
