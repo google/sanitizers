7f15f7e5655ab385b833ec31f10e551f439f979d
Vitaly Buka <vitalybuka@google.com>
2024-04-16 10:07:57 -0700

    Update start_script.sh

--- buildbot/start_script.sh
+++ buildbot/start_script.sh
@@ -157,7 +157,7 @@ function is_worker_connected() {
 }
 
 function shutdown_maybe() {
-  [[ $(cat /proc/uptime | grep -oP "^\d+") -lt 3600 ]] && return
+  [[ $(cat /proc/uptime | grep -oP "^\d+") -lt $((3600*72)) ]] && return
   (w -h | wc -l) && return
   while sudo pkill -SIGHUP buildbot-worker; do sleep 5; done;
   shutdown now
